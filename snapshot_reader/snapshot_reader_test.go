dbbf2e68a7faf2b2890d7002dbce17a68d067524
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-08-31 11:32:09 -0700

    test todos

--- snapshot_reader/snapshot_reader_test.go
+++ snapshot_reader/snapshot_reader_test.go
@@ -3,5 +3,10 @@ package snapshot_reader
 import "testing"
 
 func TestGetRow(t *testing.T) {
-	// todo
+	// todo write record
+	// todo create snapshot reader
+	// todo read row that exists in first segment
+	// todo read row that exists in another segment
+	// todo read row that doesn't exist outside the range of the segments
+	// todo read row that could exist between items but doesn't
 }
