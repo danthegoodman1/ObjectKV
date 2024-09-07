2c10b0274741ea91b64fa72916055085c54da918
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-09-07 14:49:14 -0700

    snapshot iter test todos

---
+++ snapshot_reader/snapshot_iter_test.go
@@ -0,0 +1,11 @@
+package snapshot_reader
+
+import "testing"
+
+func TestSnapshotIter(t *testing.T) {
+	// todo test reading forward to end
+	// todo test reading from middle to end
+
+	// todo test reading backward to end
+	// todo test reading from middle to end
+}
